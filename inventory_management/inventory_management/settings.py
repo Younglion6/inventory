960a829343cb777ad88651537886fea430206ced
Younglion6 <jonathan_drakes@hotmail.com>
2023-11-30 10:50:39 -0400

    ADDED requirements.txt

--- inventory_management/inventory_management/settings.py
+++ inventory_management/inventory_management/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env()
 SECRET_KEY = 'django-insecure-w07&5gwrdk==i^43uy8szu2ftmy_-izw!_-((jd!p-e5l)whj6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
